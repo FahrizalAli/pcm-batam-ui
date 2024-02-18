117748d44168b12ce799469a6956edf45022adc0
Frizali <m.fahrizalalipradana@gmail.com>
2024-02-18 14:10:57 +0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav class="bg-gray-800 p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="text-white font-bold text-xl">Logo</div>
+        <ul class="flex space-x-4">
+          <li><a href="#" class="text-white hover:text-gray-300">Home</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300">About</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300">Services</a></li>
+          <li><a href="#" class="text-white hover:text-gray-300">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container mx-auto mt-4">
+      <!-- Your main content here -->
+    </div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
