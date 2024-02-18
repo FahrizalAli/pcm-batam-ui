891fab425347f852dd594a0a94b202f0ffb9681b
Frizali <m.fahrizalalipradana@gmail.com>
2024-02-18 21:19:37 +0700

    install heroicons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,37 @@
 <template>
-  <div>
-    <nav class="bg-gray-800 p-4">
-      <div class="container mx-auto flex justify-between items-center">
-        <div class="text-white font-bold text-xl">Logo</div>
-        <ul class="flex space-x-4">
-          <li><a href="#" class="text-white hover:text-gray-300">Home</a></li>
-          <li><a href="#" class="text-white hover:text-gray-300">About</a></li>
-          <li><a href="#" class="text-white hover:text-gray-300">Services</a></li>
-          <li><a href="#" class="text-white hover:text-gray-300">Contact</a></li>
-        </ul>
+  <div class="custom-gradient py-2">
+    <div class="container mx-auto flex items-center justify-evenly">
+      <img class="logo" src="../assets/logomuhammadiyahorid-1-e1707738991754.png" alt="">
+      <div class="flex items-center gap-4 font-semibold">
+        <router-link to="/" class="block text-sm text-white">ORGANISASI</router-link>
+        <router-link to="/about" class="block text-sm text-white">DIREKTORI</router-link>
+        <router-link to="/contact" class="block text-sm text-white">GALERI</router-link>
+        <router-link to="/contact" class="block text-sm text-white">KAJIAN</router-link>
+        <router-link to="/contact" class="block text-sm text-white">ARTIKEL</router-link>
+        <router-link to="/contact" class="block text-sm text-white">BERITA</router-link>
       </div>
-    </nav>
-    <div class="container mx-auto mt-4">
-      <!-- Your main content here -->
+      <SearchIcon class="h-5 w-5 text-white" />
     </div>
   </div>
 </template>
-<script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 
-// export default {
-//   name: 'HomeView',
-//   components: {
-//     HelloWorld
-//   }
-// }
+<script>
+import { SearchIcon } from "@heroicons/vue/solid"
+
+export default {
+  components: {
+    SearchIcon
+  }
+}
 </script>
+
+<style scoped>
+.logo {
+  width: 270px;
+  height: auto;
+}
+
+.custom-gradient {
+  background: linear-gradient(to right, #348dd6 35%, #4299E1, #68D391);
+}
+</style>
